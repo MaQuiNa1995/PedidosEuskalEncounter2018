e96646f1a1cf09d2da6b69c7be855f4388070f7f
Christian Muñoz <maquina1995@gmail.com>
2018-05-12 01:37:49 +0200

    Preparacion del proyecto para una prueba levantando el servidor

--- src/main/java/es/maqui/euskal/backend/frontend/secundary/VaadinUI.java
+++ src/main/java/es/maqui/euskal/backend/frontend/secundary/VaadinUI.java
@@ -2,6 +2,8 @@ package es.maqui.euskal.backend.frontend.secundary;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
 @SpringUI
@@ -12,9 +14,9 @@ public class VaadinUI extends UI {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void init(VaadinRequest request) {
-		
-	}
+    @Override
+    protected void init(VaadinRequest request) {
+	    setContent(new Button("Click me", e -> Notification.show("Boton de prueba")));
+    }
 
 }
