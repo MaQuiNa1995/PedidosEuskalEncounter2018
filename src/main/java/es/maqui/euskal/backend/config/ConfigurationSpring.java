aae69d8d115a58305428f5cbad92e05eb8303fb9
Christian Muñoz <maquina1995@gmail.com>
2018-05-13 19:49:43 +0200

    añadida la parte front end

--- src/main/java/es/maqui/euskal/backend/config/ConfigurationSpring.java
+++ src/main/java/es/maqui/euskal/backend/config/ConfigurationSpring.java
@@ -1,12 +1,9 @@
 package es.maqui.euskal.backend.config;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @Configuration
 public class ConfigurationSpring {
 
---
+++ src/main/java/es/maqui/euskal/backend/frontend/principal/Principal.java
@@ -0,0 +1,36 @@
+package es.maqui.euskal.backend.frontend.principal;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class Principal extends UI {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void init(VaadinRequest request) {
+	    setContent(new Button("Click me", e -> Notification.show("Boton de prueba")));
+    }
+    
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = Principal.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+    }
+
+}
