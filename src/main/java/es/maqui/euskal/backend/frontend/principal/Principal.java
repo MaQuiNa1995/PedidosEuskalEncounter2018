c8f0c0d2dd3b4753d1cc3a3aabd0e158a9437ed0
Christian Muñoz <maquina1995@gmail.com>
2018-05-13 20:31:04 +0200

    quitadas dependecias sobrantes y acabado de la interfaz de usuario

--- src/main/java/es/maqui/euskal/backend/frontend/principal/Principal.java
+++ src/main/java/es/maqui/euskal/backend/frontend/principal/Principal.java
@@ -2,25 +2,27 @@ package es.maqui.euskal.backend.frontend.principal;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
 @SpringUI
+@Title("Pedidos Euskal 2018")
 public class Principal extends UI {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
 
     @Override
     protected void init(VaadinRequest request) {
-	    setContent(new Button("Click me", e -> Notification.show("Boton de prueba")));
+	    setContent(new VentanaPedidos());
     }
     
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
--- src/main/java/es/maqui/euskal/backend/frontend/principal/VentanaPedidos.java
+++ src/main/java/es/maqui/euskal/backend/frontend/principal/VentanaPedidos.java
@@ -1,5 +1,75 @@
 package es.maqui.euskal.backend.frontend.principal;
 
-public class VentanaPedidos {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.logging.Logger;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class VentanaPedidos extends VerticalLayout {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Logger LOG = Logger.getLogger(VentanaPedidos.class.getName());
+
+	private TextField textUsuario;
+	private TextArea areaPedidos;
+	private Button buttonEnviar;
+
+	public VentanaPedidos() {
+		instanciarComponentes();
+
+		buttonEnviar.addClickListener(e -> {
+
+			String nombreUsuario = textUsuario.getValue();
+			String pedidosLista = areaPedidos.getValue();
+
+			if (!nombreUsuario.isEmpty() && (pedidosLista.isEmpty())) {
+				File ficheroPedidos = new File(nombreUsuario);
+
+				try (BufferedWriter bw = new BufferedWriter(new FileWriter(ficheroPedidos, true))) {
+
+					bw.write(pedidosLista);
+
+				} catch (Exception exception) {
+					LOG.warning("Liada Parda: " + exception.getMessage());
+				}
+			}else {
+				Notification.show("Ni el nombre ni los pedidos pueden ser nulos");
+			}
+		});
+
+		this.addComponents(textUsuario, areaPedidos, buttonEnviar);
+	}
+
+	private void instanciarComponentes() {
+		textUsuario = new TextField("Pon aqui tu nombre");
+		textUsuario.setWidth(40f, Unit.PERCENTAGE);
+		areaPedidos = new TextArea("Pon aqui tu lista, cada pedido en 1 linea");
+		areaPedidos.setWidth(100f, Unit.PERCENTAGE);
+		areaPedidos.setHeight(50, Unit.PERCENTAGE);
+		buttonEnviar = new Button("Enviar a Maqui");
+
+	}
+
+	public TextField getTextUsuario() {
+		return textUsuario;
+	}
+
+	public TextArea getAreaPedidos() {
+		return areaPedidos;
+	}
+
+	public Button getButtonEnviar() {
+		return buttonEnviar;
+	}
 
 }
