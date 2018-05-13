4cdd273c51f2e5475764dc95f06e880f77dfe182
Christian Muñoz <maquina1995@gmail.com>
2018-05-13 19:47:04 +0200

    Quitado la parte de servicios un programa tan simple no necesita de estas cosas

--- src/main/java/es/maqui/euskal/backend/config/ConfigurationSpring.java
+++ src/main/java/es/maqui/euskal/backend/config/ConfigurationSpring.java
@@ -1,18 +1,20 @@
 package es.maqui.euskal.backend.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import es.maqui.euskal.backend.repository.PedidoRepository;
-
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @Configuration
 public class ConfigurationSpring {
-	
+
 	@Bean
-	public CommandLineRunner loadData(PedidoRepository repository) {
+	public CommandLineRunner loadData() {
 		return (args) -> {
-			
+
 		};
 	}
+
 }
--- src/main/java/es/maqui/euskal/backend/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package es.maqui.euskal.backend.repository;
-
-import es.maqui.euskal.backend.model.Pedido;
-
-public interface PedidoRepository extends RepositorioGenerico<Long, Pedido> {
-
-}
